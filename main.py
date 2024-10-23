3cb2b82bf2e1901ec905f51733fcf953885ca50a
DOGA3228 <vladikpelusenko@gmail.com>
2024-10-23 10:16:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import cianparser
+
+# moscow_parser = cianparser.CianParser(location="Жуковский")
+# data = moscow_parser.get_flats(deal_type="sale", rooms=(1, 2), with_saving_csv=True, additional_settings={"start_page":1, "end_page":1000})
+# print( cianparser.list_locations())
+
+
+
